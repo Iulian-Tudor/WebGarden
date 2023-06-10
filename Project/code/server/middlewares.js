91c3531f4d5cc0179b1b2b5e196c6a2c2053e811
Turtureanu Cosmin <cosminturtureanu1234@gmail.com>
2023-06-10 19:00:15 +0300

    Structured some more

---
+++ Project/code/server/middlewares.js
@@ -0,0 +1,36 @@
+import { connectToDb } from "./db.js";
+import cookie from 'cookie';
+
+function requireAuth(next) {
+    // middleware to check for auth
+    return async (req, res) => {
+        const { db, client } = await connectToDb();
+
+        let userSession = null;
+
+        try {
+            if(!req.headers.cookie) {
+                return null;
+            }
+    
+            const cookies = cookie.parse(req.headers.cookie);
+            const sessionToken = cookies['X-WEBGA-TOKEN'];
+    
+            if(!sessionToken) {
+                return null;
+            }
+    
+            userSession = await db.collection('user_sessions').findOne({token: sessionToken});
+        } finally {
+            client.close();
+        }
+
+        if(userSession === null) {
+            res.statusCode = 403;
+            return res.end("Not authorized");
+        }
+        return next(req, res, userSession);
+    };
+}
+
+export { requireAuth };
