cfc4b9838e87ee882e133f7b74eb918bd6bbe19a
Turtureanu Cosmin <cosminturtureanu1234@gmail.com>
2023-06-18 21:46:13 +0300

    Check for auth in html

--- Project/code/server/settings.js
+++ Project/code/server/settings.js
@@ -2,13 +2,19 @@ const PORT = 3000;
 
 const SESSION_TTL = 60 * 60 * 24 * 7; // 1 week
 
-const EMAIL_CONFIG = {
-    host: "smtp.gmail.com",
-    port: 587,
-    tls: {
-        rejectUnauthorized: true,
-        minVersion: "TLSv1.2"
-    }
-}
+const WHITELIST = [
+    '/html/index.html',
+    '/html/login.html',
+    '/html/signup.html',
+    '/html/reset.html',
+    '/html/resetPass.html',
+    '/html/passwordChanged.html',
+    '/html/passwordEmail.html',
+    '/html/scholarly.html',
+    '/html/about.html',
+    '/html/checkEmail.html',
+    '/html/emailChecked.html',
+    '/html/emailAlreadyChecked.html',
+];
 
-export { PORT, SESSION_TTL, EMAIL_CONFIG };
+export { PORT, SESSION_TTL, WHITELIST };
